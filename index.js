0aafb5f9eb1b01fa02ff16ba678de93484b4a1d7
hanaljufri <60494167+hanaljufri@users.noreply.github.com>
2022-06-09 17:25:16 +0800

    not fully done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-d3.select("body").append("p").text("Hello World!");
+fetch ( 'https://data.gov.sg/api/action/datastore_search?resource_id=83c21090-bd19-4b54-ab6b-d999c251edcf&')
+                .then(response => response.json())
+                .then(data =>  {
+                    console.log(data)
+                    items = data.result.records
+                    console.log(items)
+                    for (var i=0; i<items.length; i++){
+                        if(items[i].year=="2020"){
+                            console.log(items[i].year)
+                            console.log(items[i].level_2)
+                            console.log(items[i].value)
+                        }
+                    }
+
+                })
